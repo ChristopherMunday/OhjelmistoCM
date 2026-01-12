006d3203626f773bff037e3107e241e2b4d484d4
Christmu <Christopher.munday@metropolia.fi>
2026-01-12 14:20:01 +0200

    Ohjelmisto01 ensimmäiset tehtävät

---
+++ mod-01/Eka testi.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("I dont know what im doing")
+
+# kysytään käyttäjältä tämä nimi
+# otetaan saatu vastaus talteen muuttujaan,
+# jonka nimenä on nimi
+nimi = input("mikä on nimesi? ")
+# tulostus, jossa on mukana käyttäjän nimi
+print("Moi " + nimi + ", mitä kuuluu?")
