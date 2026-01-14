71360cadbc0e90b7925fd4bdc95bcfe4ec763041
Christmu <Christopher.munday@metropolia.fi>
2026-01-14 12:42:58 +0200

    Ohjelmisto01 ensimmäiset tehtävät

---
+++ Mod-02/Harjoitustehtävä01.py
@@ -0,0 +1,6 @@
+print("Hei Maailma!")
+
+nimi = input("mikä on nimesi? ")
+print("Moi " + nimi + ", mitä kuuluu?")
+
+
---
+++ Mod-02/Harjoitustehtävä02.py
@@ -0,0 +1,4 @@
+r_str = input("Mikä on ympyrän säde?")
+r = float(r_str)
+pa = (r*r)*3.14159
+print("ympyrän pinta-ala on: " +str(pa))
