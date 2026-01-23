d5cb650f772e9b41527be0e511157cce9648867b
Christmu <Christopher.munday@metropolia.fi>
2026-01-23 09:23:57 +0200

    Ohjelmisto01 ensimmäiset tehtävät

---
+++ Mod-04/Tehtävä-02.py
@@ -0,0 +1,9 @@
+# Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
+# Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
+
+tuuma = int(input("Kuinka monta tuumaa haluat että muunnan? "))
+
+tuuma = 2,54
+
+while 1 >= 1:
+    print("moi")
