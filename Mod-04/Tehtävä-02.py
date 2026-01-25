74350ccfe170796f8b917e759e1b71a5d097de78
Christmu <Christopher.munday@metropolia.fi>
2026-01-25 13:05:24 +0200

    Ohjelmisto01 ensimmäiset tehtävät

--- Mod-04/Tehtävä-02.py
+++ Mod-04/Tehtävä-02.py
@@ -1,9 +1,14 @@
 # Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
 # Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
 
-tuuma = int(input("Kuinka monta tuumaa haluat että muunnan? "))
+while True:
+    tuumat = float(input("Kuinka monta tuumaa haluat että muunnan? "))
+    if tuumat >= 0:
+        print(f"{tuumat * 2.54}cm")
+    else:
+        print("Ohjelma lopetettu!")
+        break
 
-tuuma = 2,54
-
-while 1 >= 1:
-    print("moi")
+# ensin tein while silmukan (niin kauan kun totta) sitten kysyn käyttäjältä tuuma määrää jonka jälkeen käytin
+# if ja else rakenteita muuntamaan tuumat senttimetreiksi tai lopettaa ohjelman riippuen käyttäjän vastauksesta
+# lopuksi käytin break rakennetta lopettaa ohjelman jos käyttäjän vastaus ei ole positiivinen tuuma määrä
