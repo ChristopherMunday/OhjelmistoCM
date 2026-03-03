dcae9b3fa22ea75346f6c75b3be804e4b4d28d78
Christmu <Christopher.munday@metropolia.fi>
2026-03-03 12:42:00 +0200

    Ohjelmisto01 ensimmäiset tehtävät

--- Mod-02/Harjoitustehävä05.py
+++ Mod-02/Harjoitustehävä05.py
@@ -11,8 +11,10 @@ luodit = float(luodit_str)
 # muunnan muuttujat float muotoihin
 
 luodit = leviskat*20*32+naulat*32
-massa = (luodit*13.3)/1000
-print(f"Massa on {massa: .3f} Kilogrammaa")
+grammat = luodit * 13.3
+kilogrammat = int(grammat)/1000
+grammoissa = grammat - kilogrammat * 1000
+print(f"Massa on {kilogrammat: .0f} Kilogrammaa " + str(grammoissa))
 
 # laskin luotien yhteis summan ja siitä laskin massan
 # muunsin massan grammoista kilogrammoiksi ja merkitsin sen 3 desimaalin tarkkuudella
